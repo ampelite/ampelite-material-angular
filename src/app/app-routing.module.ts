b914c56b5134ca7370846a987d00aab03c3fbce5
Phichat K <phichat_dk@hotmail.com>
2018-02-14 11:43:49 +0700

    com

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [
   { path: 'home', component: HomepageComponent, canActivate: [AuthGuard] },
   { path: 'sign-in', component: SignInComponent },
   { path: 'categories', redirectTo: '/components/categories' },
-  // {path: 'guides', component: GuideList, data: {}},
   { path: 'guide/cdk-table', redirectTo: '/cdk/table/overview' },
   // {path: 'guide/:id', component: GuideViewer, data: {}},
   {
@@ -22,7 +21,7 @@ const routes: Routes = [
     component: SidenavComponent,
     children: [
       { path: '', redirectTo: 'categories', pathMatch: 'full' },
-      { path: 'component/:id', redirectTo: ':id', pathMatch: 'full' },
+      { path: '  component/:id', redirectTo: ':id', pathMatch: 'full' },
       { path: 'category/:id', redirectTo: '/categories/:id', pathMatch: 'full' },
       {
         path: 'categories',
@@ -34,13 +33,13 @@ const routes: Routes = [
       {
         path: ':id',
         component: PageViewerComponent,
-        // children: [
-        //   { path: '', redirectTo: 'overview', pathMatch: 'full' },
-        //   { path: 'overview', component: ComponentOverview, pathMatch: 'full' },
-        //   { path: 'api', component: ComponentApi, pathMatch: 'full' },
-        //   { path: 'examples', component: ComponentExamples, pathMatch: 'full' },
-        //   { path: '**', redirectTo: 'overview' },
-        // ],
+        children: [
+          { path: '', redirectTo: 'overview', pathMatch: 'full' },
+          // { path: 'overview', component: ComponentOverview, pathMatch: 'full' },
+          // { path: 'api', component: ComponentApi, pathMatch: 'full' },
+          // { path: 'examples', component: ComponentExamples, pathMatch: 'full' },
+          // { path: '**', redirectTo: 'overview' },
+        ],
       },
     ],
   },
