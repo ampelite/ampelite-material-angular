50e9e3139c730151fe5df64760c66f3a717117ad
Phichat K <phichat_dk@hotmail.com>
2018-02-15 17:18:25 +0700

    install chartjs

--- src/app/components/po-daily/po-daily.component.ts
+++ src/app/components/po-daily/po-daily.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Params, RouterModule } from '@angular/router';
 import { DocItem, DocumentationItems, SECTIONS } from '../../shared/documentation-items/documentation-items';
@@ -6,7 +6,8 @@ import { ComponentPageTitle } from '../../pages/page-title/page-title';
 import { Observable } from 'rxjs/Observable';
 import { combineLatest } from 'rxjs/observable/combineLatest';
 import { Subscription } from 'rxjs/Subscription';
-import { map } from 'rxjs/operators/map';
+// import { map } from 'rxjs/operators/map';
+import { Chart } from 'chart.js'
 
 @Component({
   selector: 'app-po-daily',
@@ -14,6 +15,7 @@ import { map } from 'rxjs/operators/map';
   styleUrls: ['./po-daily.component.scss']
 })
 export class PoDailyComponent implements OnInit, OnDestroy {
+
   params: Observable<Params>;
   routeParamSubscription: Subscription;
   componentDocItem: DocItem;
@@ -22,8 +24,54 @@ export class PoDailyComponent implements OnInit, OnDestroy {
     public _componentPageTitle: ComponentPageTitle,
     private _route: ActivatedRoute) { }
 
+  @ViewChild('myChart') _myChart: ElementRef;
   ngOnInit() {
-    this._componentPageTitle.title = 'Po. daily report'
+    this._componentPageTitle.title = 'Po. daily report';
+
+    let myChart = new Chart(this._myChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+              stacked: true
+          }],
+          yAxes: [{
+              stacked: true
+          }]
+      }
+        // scales: {
+        //   yAxes: [{
+        //     ticks: {
+        //       beginAtZero: true
+        //     }
+        //   }]
+        // }
+      }
+    })
   }
 
   ngOnDestroy() {
