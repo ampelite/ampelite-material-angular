1170018e00337828b06e1a9bbc8ad25cb250808d
Phichat K <phichat_dk@hotmail.com>
2018-02-15 18:15:34 +0700

    update po-daily component
    add chart

--- src/app/components/po-daily/po-daily.component.ts
+++ src/app/components/po-daily/po-daily.component.ts
@@ -28,49 +28,47 @@ export class PoDailyComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this._componentPageTitle.title = 'Po. daily report';
 
+    const data = {
+      datasets: [{
+        label: 'Ampelite D-lite',
+        data: [10, 20, 30, 40],
+        // backgroundColor: 'rgba(66, 165, 245, 0.6)'
+      }, {
+        label: 'Ampelite Amperam',
+        data: [10, 20, 30, 40],
+        type: 'bar',
+        // backgroundColor: "rgba(102, 187, 106, 0.6)"
+      }, {
+        label: 'Avg',
+        data: [14, 53, 40, 20],
+        type: 'line',
+        backgroundColor: 'rgba(255, 202, 40, 0.4)',
+        borderColor: 'rgb(255, 202, 40)'
+      }, {
+        label: 'ACCU',
+        data: [10, 20, 30, 40],
+        type: 'line',
+        backgroundColor: 'rgba(236, 64, 122, 0.4)',
+        borderColor: 'rgb(236, 64, 122)'
+      }],
+      labels: ['January', 'February', 'March', 'April']
+    }
+
+    const option = {
+      scales: {
+        xAxes: [{
+          stacked: true
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    }
+
     let myChart = new Chart(this._myChart.nativeElement, {
       type: 'bar',
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          xAxes: [{
-              stacked: true
-          }],
-          yAxes: [{
-              stacked: true
-          }]
-      }
-        // scales: {
-        //   yAxes: [{
-        //     ticks: {
-        //       beginAtZero: true
-        //     }
-        //   }]
-        // }
-      }
+      data: data,
+      options: option
     })
   }
 
