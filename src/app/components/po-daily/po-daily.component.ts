91def1baad8157acc13c264d3551f9f913033372
Phichat K <phichat_dk@hotmail.com>
2018-02-20 08:28:54 +0700

    commit

--- src/app/components/po-daily/po-daily.component.ts
+++ src/app/components/po-daily/po-daily.component.ts
@@ -28,49 +28,94 @@ export class PoDailyComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this._componentPageTitle.title = 'Po. daily report';
 
-    const data = {
-      datasets: [{
-        label: 'Ampelite D-lite',
-        data: [10, 20, 30, 40],
-        backgroundColor: 'rgba(66, 165, 245, 0.6)',
-        borderColor: 'rgb(66, 165, 245)'
-      }, {
-        label: 'Ampelite Amperam',
-        data: [10, 20, 30, 40],
-        type: 'bar',
-        backgroundColor: 'rgba(102, 187, 106, 0.6)',
-        borderColor: 'rgb(102, 187, 106)'
-      }, {
-        label: 'Avg',
-        data: [14, 53, 40, 20],
-        type: 'line',
-        backgroundColor: 'rgba(255, 202, 40, 0.4)',
-        borderColor: 'rgb(255, 202, 40)'
-      }, {
-        label: 'ACCU',
-        data: [10, 20, 30, 40],
-        type: 'line',
-        backgroundColor: 'rgba(236, 64, 122, 0.4)',
-        borderColor: 'rgb(236, 64, 122)'
-      }],
-      labels: ['January', 'February', 'March', 'April']
+    const label = [];
+    for (let i = 1; i < 31; i++) {
+      label.push(i);
+    }
+
+    const dataset = {
+      datasets: [
+        {
+          label: 'Avg',
+          data: [20, 20, 20, 20],
+          type: 'line',
+          backgroundColor: 'rgba(255, 202, 40, 0.4)',
+          borderColor: 'rgb(255, 202, 40)'
+        }, {
+          label: 'Ampelite D-lite',
+          data: [10, 20, 30, 40],
+          backgroundColor: 'rgba(75, 192, 192, 0.7)',
+          borderColor: 'rgb(75, 192, 192)'
+        }, {
+          label: 'Ampelite Amperam',
+          data: [10, 28, 10, 10],
+          type: 'bar',
+          backgroundColor: 'rgba(255, 99, 132, 0.7)',
+          borderColor: 'rgb(255, 99, 132)'
+        }, {
+          yAxisID: 'accu',
+          label: 'ACCU',
+          data: [30, 20, 30, 3],
+          type: 'line',
+          backgroundColor: 'rgba(54, 162, 235, 0.4)',
+          borderColor: 'rgb(54, 162, 235)'
+        }],
+      labels: label
     };
 
     const option = {
       scales: {
-        xAxes: [{
-          stacked: true
-        }],
-        yAxes: [{
-          stacked: true
-        }]
+        xAxes: [
+          {
+            stacked: true,
+            gridLines: {
+              display: false
+            }
+          }
+        ],
+        yAxes: [
+          {
+            stacked: true
+          }, {
+            id: 'accu',
+            position: 'right',
+            gridLines: {
+              display: false
+            }
+          }
+        ]
+      },
+      animation: {
+        onComplete: function () {
+          const chartInstance = this.chart,
+            ctx = chartInstance.ctx;
+
+          // ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'bottom';
+
+          this.data.datasets.forEach(function (dataset, i) {
+            const meta = chartInstance.controller.getDatasetMeta(i);
+            meta.data.forEach(function (bar, index) {
+              const data = dataset.data[index];
+              ctx.fillText(data, bar._model.x, bar._model.y - 5);
+            });
+          });
+        }
+      },
+      legend: {
+        position: 'bottom'
+      },
+      title: {
+        display: true,
+        text: 'Custom Chart Title'
       }
     };
 
     const myChart = new Chart(this._myChart.nativeElement, {
       type: 'bar',
-      data: data,
-      options: option
+      data: dataset,
+      options: option,
     });
   }
 
