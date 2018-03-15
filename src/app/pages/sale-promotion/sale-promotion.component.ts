2502525a3df5aca8571470b2ec224081451295ae
Phichat K <phichat_dk@hotmail.com>
2018-03-15 13:07:22 +0700

    edit side nav

--- src/app/pages/sale-promotion/sale-promotion.component.ts
+++ src/app/pages/sale-promotion/sale-promotion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sale-promotion',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalePromotionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+    // window.location.href ='http://203.154.45.40/Salepromotion/main';
   }
 
 }
